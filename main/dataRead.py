e8a39a0ad2824db7735420114519e65da43bb631
Duh <ping-jung.duh.21@ucl.ack.uk>
2024-05-09 15:08:13 +0100

    update dataRead file

--- main/dataRead.py
+++ main/dataRead.py
@@ -1,6 +1,5 @@
 import os
 import numpy as np
-import pandas as pd
 import scipy.io as sio
 
 
@@ -9,8 +8,8 @@ import scipy.io as sio
 read patients quantified output from matlab folder
 e.g. '/MATLAB/Lesion_Quantification_Toolkit/Test_Outputs/Test_Batch_AAL'
 	 'aal_rp001_'
-	 'Tract_Disconnection'
-	 'aal_rp001__percent_discon_tracts.mat'
+	 'Parcel_Disconnection'
+	 'p001_AAL_percent_parcel_spared_SC.mat'
 """
 def read_mat_file(file_root_dir, patient_code, data_dir, file_name):
 #combine different output folder & file name
@@ -23,75 +22,23 @@ def read_mat_file(file_root_dir, patient_code, data_dir, file_name):
 
 	return mat_contents
 
-#read Tract Disconnection percentage: tract_discon, tract_name
-#return pandas data frame- rows: patients, col: track name
-def load_TrackDisconnect(mat_file_root_dir, patient_num):
-	data_dir = 'Tract_Disconnection'
-	file_name = '_percent_discon_tracts.mat'
-	tract_dict = {}
-
-	#for loop to read all 
-	for i in range(1, patient_num+1):
-		#different patient code
-		if i < 10:
-			patient_code = 'aal_rp00'+str(i)+'_'
-		else:
-			patient_code =  'aal_rp0'+str(i)+'_'
-
-		mat_contents=read_mat_file(mat_file_root_dir, patient_code, data_dir, file_name)
-		tract_name = mat_contents['tract_name']
-		tract_discon_val = mat_contents['tract_discon']
-
-		if i == 1:
-			for j, name in enumerate(tract_name):
-				tract_dict[name[0][0]] = tract_discon_val[j]
-		else:
-			for j, name in enumerate(tract_name):
-				tract_dict[name[0][0]]= np.append(tract_dict[name[0][0]],tract_discon_val[j])
-
-	return pd.DataFrame(data=tract_dict)
-
-
-#read disconnection matrix
-#return NxNxP ndarry
-def load_DisconnectMatrix(mat_file_root_dir, patient_num):
-	data_dir = 'Parcel_Disconnection'
-	file_name = '_Yeo7100_percent_parcel_SDC.mat'
-
-	#for loop to read all 
-	for i in range(1, patient_num+1):
-		#different patient code
-		if i < 10:
-			patient_code = 'aal_rp00'+str(i)+'_'
-		else:
-			patient_code =  'aal_rp0'+str(i)+'_'
-
-		mat_contents = read_mat_file(mat_file_root_dir, patient_code, data_dir, file_name)
-		discon_matrix = mat_contents['pct_sdc_matrix']
-
-		if i == 1:
-			discon_Matrix = np.array(discon_matrix)
-		elif i == 2: 
-			discon_Matrix = np.stack([discon_Matrix, discon_matrix], axis=-1)
-		else:
-			discon_Matrix = np.concatenate((discon_Matrix, discon_matrix[..., np.newaxis]), axis=-1)
-	
-	return discon_Matrix
-
 
 #read spared (residual) connection matrix
 #return NxNxP ndarray
 def loadSparedConnectMatrix(mat_file_root_dir, patient_num):
 	data_dir = 'Parcel_Disconnection'
-	file_name = '_Yeo7100_percent_parcel_spared_SC.mat'
+	file_name = '_AAL_percent_parcel_spared_SC.mat'
+	#file_name = '_Yeo7100_percent_parcel_spared_SC.mat'
 
 	#for loop to read all 
 	for i in range(1, patient_num+1):
 		#different patient code
 		if i < 10:
-			patient_code = 'aal_rp00'+str(i)+'_'
+			patient_code = 'p00'+str(i)+'_'
+			#patient_code = 'aal_rp00'+str(i)+'_'
 		else:
-			patient_code =  'aal_rp0'+str(i)+'_'
+			patient_code =  'p0'+str(i)+'_'
+			#patient_code =  'aal_rp0'+str(i)+'_'
 
 		mat_contents = read_mat_file(mat_file_root_dir, patient_code, data_dir, file_name)
 		spared_con_matrix = mat_contents['pct_spared_sc_matrix']
